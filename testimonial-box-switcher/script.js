74bfc93b2a086e5f6441977bc0db8d38d69f6575
Suraj Shedge <sshedge@qti.qualcomm.com>
2026-08-26 15:58:39 +0530

    Fix testimonial-box-switcher: correct container selector
    script.js queried .testimonials-container (plural), but the HTML
    element is .testimonial-container (singular), so the lookup always
    returned null.
    
    Change-Id: Idefe263a0df6913c342c88d25895029d6b597abe

--- testimonial-box-switcher/script.js
+++ testimonial-box-switcher/script.js
@@ -1,4 +1,4 @@
-const testimonialsContainer = document.querySelector('.testimonials-container')
+const testimonialsContainer = document.querySelector('.testimonial-container')
 const testimonial = document.querySelector('.testimonial')
 const userImage = document.querySelector('.user-image')
 const username = document.querySelector('.username')
