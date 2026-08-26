6547f8492565a907a8f32e92f5ea3db6ae486641
Suraj Shedge <sshedge@qti.qualcomm.com>
2026-08-26 15:57:37 +0530

    Fix custom-range-slider: position label on initial load
    The label's position was only ever computed inside the input event
    handler, so it relied on the hardcoded left: 110px in style.css
    coincidentally matching the slider's default value. Extract the
    logic into updateLabel() and call it once on load too.
    
    Change-Id: I6814f0c08dc9328a68f3404a180b5e87b37f0a4f

--- custom-range-slider/script.js
+++ custom-range-slider/script.js
@@ -1,17 +1,19 @@
 const range = document.getElementById('range')
 
-range.addEventListener('input', (e) => {
-    const value = +e.target.value
-    const label = e.target.nextElementSibling
+range.addEventListener('input', (e) => updateLabel(e.target))
 
-    const range_width = getComputedStyle(e.target).getPropertyValue('width')
+function updateLabel(target) {
+    const value = +target.value
+    const label = target.nextElementSibling
+
+    const range_width = getComputedStyle(target).getPropertyValue('width')
     const label_width = getComputedStyle(label).getPropertyValue('width')
 
     const num_width = +range_width.substring(0, range_width.length - 2)
     const num_label_width = +label_width.substring(0, label_width.length - 2)
 
-    const max = +e.target.max
-    const min = +e.target.min
+    const max = +target.max
+    const min = +target.min
 
     const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10)
 
@@ -19,9 +21,11 @@ range.addEventListener('input', (e) => {
 
 
     label.innerHTML = value
-})
+}
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
+
+updateLabel(range)
